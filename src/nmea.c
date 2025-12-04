1d5939d0885ca5e81e1ba82c0cf63da9dd7f1590
Tvirus <tvirus347@gmail.com>
2025-12-04 17:05:44 +0800

    fix and optimize nmea

--- src/nmea.c
+++ src/nmea.c
@@ -69,6 +69,21 @@ static char* check_sentence(const char *str)
     }
 }
 
+static void str_sep(const char *str, char **p, unsigned int max)
+{
+    while (('*' != *str) && ('\r' != *str) && ('\n' != *str) && ('\0' != *str) && max)
+    {
+        if (',' == *str)
+        {
+            *p++ = (char *)str;
+            max--;
+        }
+        str++;
+    }
+    while (max--)
+        *p++ = NULL;
+}
+
 static char* str_to_int(const char *str, unsigned char exponent, int32_t *out)
 {
     unsigned char is_negative;
@@ -117,19 +132,14 @@ static char* str_to_int(const char *str, unsigned char exponent, int32_t *out)
         decimal = decimal * 10 + *p++ - '0';
     }
     for (; i < exponent; i++)
-    {
         decimal *= 10;
-    }
 
     *out = (int32_t)integer * factor + (int32_t)decimal;
     if (is_negative)
         *out = -(*out);
 
     while (('0' <= *p) && ('9' >= *p))
-    {
         p++;
-    }
-
     return p;
 }
 
@@ -162,9 +172,7 @@ static char* str_dm_to_d(const char *dm, unsigned char exponent, int64_t *degree
         m2 = m2 * 10 + *p++ - '0';
     }
     for (; i < exponent; i++)
-    {
         m2 *= 10;
-    }
 
     d = integer / 100;
     m1 = integer % 100;
@@ -181,25 +189,33 @@ static char* str_dm_to_d(const char *dm, unsigned char exponent, int64_t *degree
 
     return p + 1;
 }
+
 static int nmea_parse_gga(const char *str, format_gga_t *gga)
 {
-    const char *p;
-    int32_t v;
+    char *p[14];
+    int32_t val;
+    char c;
 
-    if (',' != *str)
+    str_sep(str, p, sizeof(p) / sizeof(p[0]));
+
+    /* time */
+    if (NULL == p[0])
     {
-        v = -1;
-        p = str_to_int(str, 3, &v);
-        if ((NULL == p) || (0 > v) || (235959999 < v))
+        LOG(LOGLEVEL_ERROR, "gga time not exist !");
+        return -1;
+    }
+    if (',' != *(p[0] + 1))
+    {
+        if ((NULL == str_to_int(p[0] + 1, 3, &val)) || (0 > val) || (235959999 < val))
         {
-            LOG(LOGLEVEL_ERROR, "gga time err !");
+            LOG(LOGLEVEL_ERROR, "gga time format err !");
             return -1;
         }
-        gga->utc_hour = v / 10000000;
-        gga->utc_minute = v / 100000 - (v / 10000000) * 100;
-        gga->utc_second = v / 1000 - (v / 100000) * 100;
-        gga->utc_usecond = v - (v / 1000) * 1000;
-        str = p;
+        gga->utc_hour = val / 10000000;
+        gga->utc_minute = val / 100000 - (val / 10000000) * 100;
+        gga->utc_second = val / 1000 - (val / 100000) * 100;
+        gga->utc_usecond = val - (val / 1000) * 1000;
+        gga->time_valid = 1;
     }
     else
     {
@@ -208,123 +224,122 @@ static int nmea_parse_gga(const char *str, format_gga_t *gga)
     }
 
     /* latitude */
-    if (',' != *str++)
+    if (NULL == p[1])
     {
         LOG(LOGLEVEL_ERROR, "gga latitude not exist !");
         return -1;
     }
-    if (',' != *str)
+    if (',' != *(p[1] + 1))
     {
-        p = str_dm_to_d(str, 9, &gga->latitude);
-        if (NULL == p)
-        {
-            LOG(LOGLEVEL_ERROR, "gga latitude err !");
-            return -1;
-        }
-        str = p;
-    }
-    else
-    {
-        if (',' != *++str)
+        if (NULL == str_dm_to_d(p[1] + 1, 9, &gga->latitude))
         {
             LOG(LOGLEVEL_ERROR, "gga latitude format err !");
             return -1;
         }
+        gga->latitude_valid = 1;
+    }
+    else
+    {
         LOG(LOGLEVEL_DEBUG, "gga latitude is none");
         gga->latitude_valid = 0;
     }
 
     /* longitude */
-    if (',' != *str++)
+    if (NULL == p[3])
     {
         LOG(LOGLEVEL_ERROR, "gga longitude not exist !");
         return -1;
     }
-    if (',' != *str)
+    if (',' != *(p[3] + 1))
     {
-        p = str_dm_to_d(str, 9, &gga->longitude);
-        if (NULL == p)
-        {
-            LOG(LOGLEVEL_ERROR, "gga longitude err !");
-            return -1;
-        }
-        str = p;
-    }
-    else
-    {
-        if (',' != *++str)
+        if (NULL == str_dm_to_d(p[3] + 1, 9, &gga->longitude))
         {
             LOG(LOGLEVEL_ERROR, "gga longitude format err !");
             return -1;
         }
+        gga->longitude_valid = 1;
+    }
+    else
+    {
         LOG(LOGLEVEL_DEBUG, "gga longitude is none");
         gga->longitude_valid = 0;
     }
 
     /* quality */
-    if (',' != *str++)
+    if (NULL == p[5])
     {
         LOG(LOGLEVEL_ERROR, "gga quality not exist !");
         return -1;
     }
-    if ('0' == *str)
-        gga->quality = GGA_QUALITY_INVALID;
-    else if ('1' == *str)
-        gga->quality = GGA_QUALITY_VALID;
-    else if ('2' == *str)
-        gga->quality = GGA_QUALITY_DIFF;
-    else if ('3' == *str)
-        gga->quality = GGA_QUALITY_PPS;
-    else if ('4' == *str)
-        gga->quality = GGA_QUALITY_RTK_FIXED;
-    else if ('5' == *str)
-        gga->quality = GGA_QUALITY_RTK_FLOATING;
-    else if ('6' == *str)
-        gga->quality = GGA_QUALITY_ESTIMATED;
-    else if ('7' == *str)
-        gga->quality = GGA_QUALITY_MANUAL;
-    else if ('8' == *str)
-        gga->quality = GGA_QUALITY_SIMULATOR;
+    c = *(p[5] + 1);
+    if (',' != c)
+    {
+        if ('0' == c)
+            gga->quality = GGA_QUALITY_INVALID;
+        else if ('1' == c)
+            gga->quality = GGA_QUALITY_VALID;
+        else if ('2' == c)
+            gga->quality = GGA_QUALITY_DIFF;
+        else if ('3' == c)
+            gga->quality = GGA_QUALITY_PPS;
+        else if ('4' == c)
+            gga->quality = GGA_QUALITY_RTK_FIXED;
+        else if ('5' == c)
+            gga->quality = GGA_QUALITY_RTK_FLOATING;
+        else if ('6' == c)
+            gga->quality = GGA_QUALITY_ESTIMATED;
+        else if ('7' == c)
+            gga->quality = GGA_QUALITY_MANUAL;
+        else if ('8' == c)
+            gga->quality = GGA_QUALITY_SIMULATOR;
+        else
+        {
+            LOG(LOGLEVEL_ERROR, "unsupported gga quality: '%c' !", c);
+            return -1;
+        }
+    }
     else
     {
-        LOG(LOGLEVEL_ERROR, "unsupported gga quality: '%c' !", *str);
+        LOG(LOGLEVEL_ERROR, "gga quality is none !");
         return -1;
     }
-    str++;
 
     /* satellites */
-    if (',' != *str++)
+    if (NULL == p[6])
     {
         LOG(LOGLEVEL_ERROR, "gga satellites not exist !");
         return -1;
     }
-    v = -1;
-    p = str_to_int(str, 0, &v);
-    if ((NULL == p) || (0 > v))
+    if (',' != *(p[6] + 1))
     {
-        LOG(LOGLEVEL_ERROR, "gga satellites err !");
+        if ((NULL == str_to_int(p[6] + 1, 0, &val)) || (0 > val))
+        {
+            LOG(LOGLEVEL_ERROR, "gga satellites format err !");
+            return -1;
+        }
+        gga->satellites = (typeof(gga->satellites))val;
+    }
+    else
+    {
+        LOG(LOGLEVEL_ERROR, "gga satellites is none !");
         return -1;
     }
-    gga->satellites = (typeof(gga->satellites))v;
-    str = p;
 
     /* dhop */
-    if (',' != *str++)
+    if (NULL == p[7])
     {
         LOG(LOGLEVEL_ERROR, "gga dhop not exist !");
         return -1;
     }
-    if (',' != *str)
+    if (',' != *(p[7] + 1))
     {
-        v = -1;
-        p = str_to_int(str, 2, &v);
-        if ((NULL == p) || (0 > v))
+        if ((NULL == str_to_int(p[7] + 1, 2, &val)) || (0 > val))
         {
-            LOG(LOGLEVEL_ERROR, "gga dhop err !");
+            LOG(LOGLEVEL_ERROR, "gga dhop format err !");
             return -1;
         }
-        gga->dhop = (typeof(gga->dhop))v;
-        str = p;
+        gga->dhop = (typeof(gga->dhop))val;
+        gga->dhop_valid = 1;
     }
     else
     {
@@ -332,81 +347,75 @@ static int nmea_parse_gga(const char *str, format_gga_t *gga)
         gga->dhop_valid = 0;
     }
 
-
     /* altitude */
-    if (',' != *str++)
+    if (NULL == p[8])
     {
         LOG(LOGLEVEL_ERROR, "gga altitude not exist !");
         return -1;
     }
-    if (',' != *str)
+    if (',' != *(p[8] + 1))
     {
-        p = str_to_int(str, 3, &v);
-        if (NULL == p)
+        if (NULL == str_to_int(p[8] + 1, 3, &val))
         {
-            LOG(LOGLEVEL_ERROR, "gga altitude err !");
+            LOG(LOGLEVEL_ERROR, "gga altitude format err !");
             return -1;
         }
-        gga->altitude = (typeof(gga->altitude))v;
-        str = p;
+        gga->altitude = (typeof(gga->altitude))val;
+        gga->altitude_valid = 1;
     }
     else
     {
         LOG(LOGLEVEL_DEBUG, "gga altitude is none");
         gga->altitude_valid = 0;
     }
-    if ((',' != str[0]) || 'M' != str[1])
+    if ((NULL == p[9]) || 'M' != *(p[9] + 1))
     {
-        LOG(LOGLEVEL_ERROR, "gga altitude format err !");
+        LOG(LOGLEVEL_ERROR, "gga altitude unit err !");
         return -1;
     }
-    str += 2;
 
     /* geoidal_separation */
-    if (',' != *str++)
+    if (NULL == p[10])
     {
         LOG(LOGLEVEL_ERROR, "gga geoidal_separation not exist !");
         return -1;
     }
-    if (',' != *str)
+    if (',' != *(p[10] + 1))
     {
-        p = str_to_int(str, 3, &v);
-        if (NULL == p)
+        if (NULL == str_to_int(p[10] + 1, 3, &val))
         {
-            LOG(LOGLEVEL_ERROR, "gga geoidal_separation err !");
+            LOG(LOGLEVEL_ERROR, "gga geoidal_separation format err !");
             return -1;
         }
-        gga->geoidal_separation = (typeof(gga->geoidal_separation))v;
-        str = p;
+        gga->geoidal_separation = (typeof(gga->geoidal_separation))val;
+        gga->geoidal_separation_valid = 1;
     }
     else
     {
         LOG(LOGLEVEL_DEBUG, "gga geoidal_separation is none");
         gga->geoidal_separation_valid = 0;
     }
-    if ((',' != str[0]) || 'M' != str[1])
+    if ((NULL == p[11]) || 'M' != *(p[11] + 1))
     {
-        LOG(LOGLEVEL_ERROR, "gga geoidal_separation format err !");
+        LOG(LOGLEVEL_ERROR, "gga geoidal_separation unit err !");
         return -1;
     }
-    str += 2;
 
     /* age_of_diff_data */
-    if (',' != *str++)
+    if (NULL == p[12])
     {
         LOG(LOGLEVEL_ERROR, "gga age_of_diff_data not exist !");
         return -1;
     }
-    if (',' != *str)
+    if (',' != *(p[12] + 1))
     {
-        p = str_to_int(str, 1, &v);
-        if (NULL == p)
+        if (NULL == str_to_int(p[12] + 1, 1, &val))
         {
-            LOG(LOGLEVEL_ERROR, "gga age_of_diff_data err !");
+            LOG(LOGLEVEL_ERROR, "gga age_of_diff_data format err !");
             return -1;
         }
-        gga->age_of_diff_data = (typeof(gga->age_of_diff_data))v;
-        str = p;
+        gga->age_of_diff_data = (typeof(gga->age_of_diff_data))val;
+        gga->age_of_diff_data_valid = 1;
     }
     else
     {
@@ -417,27 +426,27 @@ static int nmea_parse_gga(const char *str, format_gga_t *gga)
     }
 
     /* diff_station_id */
-    if (',' != *str++)
+    if (NULL == p[13])
     {
         LOG(LOGLEVEL_ERROR, "gga diff_station_id not exist !");
         return -1;
     }
-    if (',' != *str)
+    if (',' != *(p[13] + 1))
     {
-        p = str_to_int(str, 0, &v);
-        if (NULL == p)
+        if (NULL == str_to_int(p[13] + 1, 0, &val))
         {
-            LOG(LOGLEVEL_ERROR, "gga diff_station_id err !");
+            LOG(LOGLEVEL_ERROR, "gga diff_station_id format err !");
             return -1;
         }
-        gga->diff_station_id = (typeof(gga->diff_station_id))v;
-        str = p;
+        gga->diff_station_id = (typeof(gga->diff_station_id))val;
+        gga->diff_station_id_valid = 1;
     }
     else
     {
         LOG(LOGLEVEL_DEBUG, "gga diff_station_id is none");
         gga->diff_station_id_valid = 0;
     }
+
     return 0;
 }
 
@@ -472,7 +481,7 @@ char* nmea_parse(const char *str, nmea_msg_t *msg)
     if (!memcmp(str, "GGA,", 4))
     {
         msg->format_id = FORMAT_ID_GGA;
-        if (nmea_parse_gga(str + 4, &msg->gga))
+        if (nmea_parse_gga(str, &msg->gga))
             return NULL;
     }
     else
